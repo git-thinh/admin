4e6773a09f44020dc9f619f49a463a911cff05e1
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2018-03-06 06:31:51 +0700

    a

--- worker.js
+++ worker.js
@@ -3,71 +3,38 @@
 var CACHE_NAME = 'dependencies-cache';
 
 self.addEventListener('install', function (event) {
-    // Perform the install step:
-    // * Load a JSON file from server
-    // * Parse as JSON
-    // * Add files to the cache list
-
     // Message to simply show the lifecycle flow
-    console.log('[install] Kicking off service worker registration!');
+    console.log('[WORKER-INSTALL] Kicking off service worker registration!');
 
-    event.waitUntil(function () { 
-
-        caches.open(CACHE_NAME)
-            .then(function (cache) {
-                // With the cache opened, load a JSON file containing an array of files to be cached
-                return fetch('cache.json').then(function (response) {
-                    // Once the contents are loaded, convert the raw text to a JavaScript object
-                    return response.json();
-                }).then(function (files) {
-                    // Use cache.addAll just as you would a hardcoded array of items
-                    console.log('[install] Adding files from JSON file: ', files);
-                    return cache.addAll(files);
-                });
-            })
-            .then(function () {
-                // Message to simply show the lifecycle flow
-                console.log(
-                    '[install] All required resources have been cached;',
-                    'the Service Worker was successfully installed!'
-                );
-
-                // Force activation
-                return self.skipWaiting();
-            })
-    }
-    );
+    event.waitUntil(self.skipWaiting()); 
 });
 
 self.addEventListener('fetch', function (event) {
     var url = event.request.url;
-    console.log('[fetch] = ' + url);
 
-    if (url.includes('/#') || url.endsWith('localhost/')) {
-        var data = '<!DOCTYPE html><html><head><title></title><meta charset="utf-8" /><meta name="viewport" content="width=device-width, initial-scale=1" />'+
-                    '<script src="app.js"></script></head><body></body></html>';
-        var resBuilder = new Response(data, {
-            headers: {
-                // As the zip says nothing about the nature of the file, we extract
-                // this information from the file name.
-                'Content-Type': 'text/html'
-            }
-        });
-        return resBuilder;
+    if (url.includes('/#')) {
+        console.log('[WORKER-FETCH] get online: ', event.request.url);
+        var init = {
+            method: 'GET',
+            cache: 'default'
+        };
+        var url_module = url.split('/#').join('/module/') + '.html';
+        console.log('[WORKER-FETCH] get module: ', url_module);
+        event.respondWith(fetch(url_module, init));
     } else {
         event.respondWith(
             caches.match(event.request)
                 .then(function (response) {
                     // Cache hit - return the response from the cached version
                     if (response) {
-                        console.log('[fetch] Returning from Service Worker cache: ', event.request.url);
+                        console.log('[WORKER-FETCH] get cache: ', event.request.url);
                         return response;
                     }
 
                     // Not in cache - return the result from the live server
                     // `fetch` is essentially a "fallback"
-                    //console.log('[fetch] Returning from server: ', event.request.url);
-                    //return fetch(event.request);
+                    console.log('[WORKER-FETCH] get online: ', event.request.url);
+                    return fetch(event.request);
                 })
         );
     }
@@ -75,12 +42,14 @@ self.addEventListener('fetch', function (event) {
 
 self.addEventListener('activate', function (event) {
     // Message to simply show the lifecycle flow
-    console.log('[activate] Activating service worker!');
+    console.log('[WORKER-ACTIVATE] Activating service worker!');
 
     // Claim the service work for this client, forcing `controllerchange` event
-    console.log('[activate] Claiming this service worker!');
+    //console.log('[WORKER-ACTIVATE] Claiming this service worker!');
     //event.waitUntil(self.clients.claim());
 
+    console.log('[WORKER-ACTIVATE] Delete outdated caches');
+    // Use this to delete outdated caches
     event.waitUntil(caches.keys().then(function (cacheNames) {
         return Promise.all(
           cacheNames.filter(function (cacheName) {
@@ -93,24 +62,6 @@ self.addEventListener('activate', function (event) {
           })
         );
     })
-  );
-
+  );// end event
 });
 
-
-var contentTypesByExtension = {
-    'css': 'text/css',
-    'js': 'application/javascript',
-    'png': 'image/png',
-    'jpg': 'image/jpeg',
-    'jpeg': 'image/jpeg',
-    'html': 'text/html',
-    'htm': 'text/html'
-};
-
-// Return the content type of a file based on the name extension
-function getContentType(filename) {
-    var tokens = filename.split('.');
-    var extension = tokens[tokens.length - 1];
-    return contentTypesByExtension[extension] || 'text/plain';
-}
