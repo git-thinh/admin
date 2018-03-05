b5dbdc392da891511ea77d0922ac7cffc3ad7b4c
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2018-03-06 05:25:37 +0700

    a

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 ﻿var _CACHE_STORE = 'CACHE_STORE';
+var _CACHE_FILE = 'cache.json';
 
-function init() {
+function init_Page() {
     // We switch between production and testing environments by checking the hash of the URL.
     window.onhashchange = function () {
         var newHash = window.location.hash.substr(1) || 'admin';
@@ -16,27 +17,63 @@ function init() {
     window.onhashchange();
 }
 
+function init_Worker() {
+    // Once the Service Worker is activated, init()
+    if ('serviceWorker' in navigator) {
+
+        navigator.serviceWorker.getRegistration().then(function (registration) {
+            if (!registration) {
+
+                navigator.serviceWorker.register('worker.js', { scope: '.' });
+                navigator.serviceWorker.ready.then(function () {
+                    init_Page();
+                });
+
+                return;
+            }
+
+            //registration.unregister().then(function () {
+            //    console.log('The application has been uninstalled service worker');
+            //    setTimeout(function () { location.reload(); }, 500);
+            //}).catch(function (error) {
+            //    console.log('Error while uninstalling the service worker:');
+            //    console.log(error.message);
+            //});
+        });
+
+        //if (navigator.serviceWorker.controller) {
+        //    init();
+        //} else {
+        //    navigator.serviceWorker.register('worker.js', { scope: '.' });
+        //    navigator.serviceWorker.ready.then(function () {
+        //        init();
+        //    });
+        //}
+    }
+}
+
 if ('caches' in window) {
-    //caches.keys().then(function (names) {
-    //    if (names.length > 0) {
-    //        for (let name of names) {
-    //            console.log('[INDEX.JS] Clean cache store: ' + name);
-    //            caches.delete(name);
-    //        }
-    //        window.location.reload();
-    //    }
-    //});
+    // Clean cache store space
+    caches.keys().then(function (names) {
+        if (names.length > 0) {
+            for (let name of names) {
+                console.log('[INDEX.JS] Clean cache store: ' + name);
+                caches.delete(name);
+            }
+            window.location.reload();
+        }
+    });
 
     //caches.open(_CACHE_STORE).then(function (cache) {
     //    console.log('[INDEX.JS] Set cache file: /index.html');
     //    cache.add('/index.html');
     //});
-
-
+    
+    // Set cache again via file cache.json
     caches.open(_CACHE_STORE)
         .then(function (cache) {
             // With the cache opened, load a JSON file containing an array of files to be cached
-            return fetch('cache.json').then(function (response) {
+            return fetch(_CACHE_FILE).then(function (response) {
                 // Once the contents are loaded, convert the raw text to a JavaScript object
                 return response.json();
             }).then(function (files) {
@@ -54,57 +91,6 @@ if ('caches' in window) {
 
             // Force activation
             //return self.skipWaiting();
-
-            window.location.reload();
-
-        })
-
-
-
-
-
-
-
-
-
-}
-
-// Once the Service Worker is activated, init()
-if ('serviceWorker' in navigator) {
-
-    ////navigator.serviceWorker.getRegistration().then(function (registration) {
-    ////    if (!registration) {
-
-    ////        navigator.serviceWorker.register('worker.js', { scope: '.' });
-    ////        navigator.serviceWorker.ready.then(function () {
-    ////            init();
-    ////        });
-
-    ////        return;
-    ////    }
-
-    ////    registration.unregister().then(function () {
-    ////        console.log('The application has been uninstalled service worker');
-    ////        setTimeout(function () { location.reload(); }, 500);
-
-    ////        //navigator.serviceWorker.register('worker.js', { scope: '.' });
-    ////        //navigator.serviceWorker.ready.then(function () {
-    ////        //    console.log('The application has been reinstalled service worker');
-    ////        //    init();
-    ////        //});
-
-    ////    }).catch(function (error) {
-    ////        console.log('Error while uninstalling the service worker:');
-    ////        console.log(error.message);
-    ////    });
-    ////});
-
-    //if (navigator.serviceWorker.controller) {
-    //    init();
-    //} else {
-    //    navigator.serviceWorker.register('worker.js', { scope: '.' });
-    //    navigator.serviceWorker.ready.then(function () {
-    //        init();
-    //    });
-    //}
-}
+            init_Worker();
+        });;
+} // end cache
