95c063e1e8879e7a73b4754b3c45616802d8ebbd
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2018-03-06 10:17:33 +0700

    nodejs

--- worker.js
+++ worker.js
@@ -1,26 +1,30 @@
-// [Working example](/serviceworker-cookbook/json-cache/).
-
 var CACHE_NAME = 'dependencies-cache';
 
 self.addEventListener('install', function (event) {
     // Message to simply show the lifecycle flow
     console.log('[WORKER-INSTALL] Kicking off service worker registration!');
 
-    event.waitUntil(self.skipWaiting()); 
+    event.waitUntil(self.skipWaiting());
 });
 
 self.addEventListener('fetch', function (event) {
+    var url_new = '';
     var url = event.request.url;
+    var host = url.split('/')[2];
+    console.log('[WORKER-FETCH-URL] = ' + url);
 
-    if (url.includes('/#')) {
-        console.log('[WORKER-FETCH] get online: ', event.request.url);
-        var init = {
-            method: 'GET',
-            cache: 'default'
-        };
+    if (url.endsWith(host + '/')) {
+        url_new = url + 'module/admin.html';
+        console.log('[WORKER-FETCH] get: ', url_new);
+        debugger;
+        event.respondWith(fetch(url_new, { method: 'GET', cache: 'default' }));
+    }
+    else if (url.includes('/#')) {
+        console.log('[WORKER-FETCH] get online: ', url);
         var url_module = url.split('/#').join('/module/') + '.html';
         console.log('[WORKER-FETCH] get module: ', url_module);
-        event.respondWith(fetch(url_module, init));
+        debugger;
+        event.respondWith(fetch(url_module, { method: 'GET', cache: 'default' }));
     } else {
         event.respondWith(
             caches.match(event.request)
@@ -28,12 +32,14 @@ self.addEventListener('fetch', function (event) {
                     // Cache hit - return the response from the cached version
                     if (response) {
                         console.log('[WORKER-FETCH] get cache: ', event.request.url);
+                        debugger;
                         return response;
                     }
 
                     // Not in cache - return the result from the live server
                     // `fetch` is essentially a "fallback"
                     console.log('[WORKER-FETCH] get online: ', event.request.url);
+                    debugger;
                     return fetch(event.request);
                 })
         );
@@ -52,16 +58,16 @@ self.addEventListener('activate', function (event) {
     // Use this to delete outdated caches
     event.waitUntil(caches.keys().then(function (cacheNames) {
         return Promise.all(
-          cacheNames.filter(function (cacheName) {
-              // Return true if you want to remove this cache,
-              // but remember that caches are shared across
-              // the whole origin
-          }).map(function (cacheName) {
-              console.log('[activate] Clean cache file: ' + cacheName);
-              return caches.delete(cacheName);
-          })
+            cacheNames.filter(function (cacheName) {
+                // Return true if you want to remove this cache,
+                // but remember that caches are shared across
+                // the whole origin
+            }).map(function (cacheName) {
+                console.log('[activate] Clean cache file: ' + cacheName);
+                return caches.delete(cacheName);
+            })
         );
     })
-  );// end event
+    );// end event
 });
 
